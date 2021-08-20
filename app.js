fcbe7e5213085e845413b09f5f5374b47c0457a0
srsamal1 <ssamal1@evoketechnologies.com>
2021-08-20 17:45:27 +0530

    comented some code

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://management-dashboard.evokeapps.com/");
+  // res.setHeader("Access-Control-Allow-Origin", "https://management-dashboard.evokeapps.com/");
 
   // Request methods you wish to allow
   res.setHeader("Access-Control-Allow-Origin", "*");
