25e993e061baed4fd131db8ab6aab8a81191d3f2
srsamal1 <ssamal1@evoketechnologies.com>
2021-08-26 18:53:29 +0530

    remove headers from app.js file

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const https = require('https');
 const path = require('path');
 const fs = require('fs');
 
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 80;
 
 var options ={
   key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
   cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
 }
 
-const sslserver = https.createServer(options,app)
-var server = sslserver.listen(port, function () {
+// const sslserver = https.createServer(options,app)
+var server = app.listen(port, function () {
   console.log(`Express server listening on port ${port}`);
 });
