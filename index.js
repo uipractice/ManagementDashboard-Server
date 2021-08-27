90b7f358bdd3872961c354c93f50b1ddcc040aeb
srsamal1 <ssamal1@evoketechnologies.com>
2021-08-27 17:24:31 +0530

    Revert the changes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var app = require("./app");
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
+// const https = require('https');
+// const path = require('path');
+// const fs = require('fs');
 
 var port = process.env.PORT || 80;
 
-var options ={
-  key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-  cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
-}
+// var options ={
+//   key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
+// }
 
-const sslserver = https.createServer(options,app)
-var server = sslserver.listen(port, function () {
+// const sslserver = https.createServer(options,app)
+var server = app.listen(port, function () {
   console.log(`Express server listening on port ${port}`);
 });
